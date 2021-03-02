947982f3086c283e9539724c24ed744766675dc1
Lucas Brito <lucasbsilva29@gmail.com>
2021-03-01 23:33:13 -0300

    style(test_aux.py): indentation

--- tests/test_aux.py
+++ tests/test_aux.py
@@ -1,10 +1,12 @@
 import unittest
 import cv2
 
+
 class TestAuxiliaryUnit(unittest.TestCase):
     def setUp(self):
         self.image_path = 'resources/images/1.jpg'
         self.cv_image = cv2.imread(self.image_path)
+
 
 if __name__ == '__main__':
     unittest.main()
