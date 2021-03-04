cce9fddea46a23e0dd37b0b5dd0e66afe567ee30
Lucas Brito <lucasbsilva29@gmail.com>
2021-03-03 22:52:57 -0300

    feat(exercise2.py): making a backbone to the second exercise

--- src/exercise2.py
+++ src/exercise2.py
@@ -8,10 +8,7 @@ from natsort import natsorted
 from matplotlib import pyplot as plt
 
 
-""" model = dlib.cnn_face_detection_model_v1(
-    "resources/model/mmod_human_face_detector.dat")
-facesDetectadas = model(imagem, 2)
-print(facesDetectadas)
+"""
 print("Faces detectadas: ", len(facesDetectadas))
 for face in facesDetectadas:
     e, t, d, b, c = (int(face.rect.left()), int(face.rect.top()), int(
@@ -21,35 +18,45 @@ for face in facesDetectadas:
 
 cv2.imshow("Detector CNN", imagem)
 cv2.waitKey(0)
-cv2.destroyAllWindows() """
+cv2.destroyAllWindows()
+"""
 
 
 def detect_faces(_path):
     pictures = glob.glob(path.join(_path, "*.jpg")).copy()
     pictures = natsorted(pictures)
-    model = MTCNN()
+    mtcnn_model = MTCNN()
+    cnn_model = dlib.cnn_face_detection_model_v1(
+        "resources/model/mmod_human_face_detector.dat")
 
     for _file in pictures:
-        img = cv2.cvtColor(cv2.imread(_file), cv2.COLOR_BGR2RGB)
-        detected_face = model.detect_faces(img)
-        if detected_face:
-            # plot_poits(img, detected_face)
-            count_detected = {i: detected.count(i) for i in detected}
-            print("MTCNN: {0} faces".format(count_detected))
-            print("CNN: {0} faces".format(count_detected))
+        mtcnn_detect(mtcnn_model, _file)
+        cnn_detect(cnn_model, _file)
 
-    return count_detected
+    return
 
 
-def plot_poits(_image, detected_face):
-    if len(detected_face):
-        x1, y1, x2, y2 = detected_face[0]["box"]
-        _image = cv2.rectangle(_image, (x1, y1), (x2, y2), (255, 0, 0), 2)
-        for point in detected_face[0]["keypoints"].values():
-            x, y = point
-            _image = cv2.circle(_image, (x, y), radius=1,
-                                color=(0, 0, 255), thickness=3)
-    return _image
+def mtcnn_detect(model, _file):
+    """
+    # MTCNN Detection
+    This function does face detection with MTCNN.
+    """
+    img = cv2.cvtColor(cv2.imread(_file), cv2.COLOR_BGR2RGB)
+    detected_faces = model.detect_faces(img)
+    if detected_faces:
+        print("MTCNN: {0} faces".format(len(detected_faces)))
+    return len(detected_faces)
+
+
+def cnn_detect(model, _file):
+    """
+    # CNN Detection
+    This function does face detection with CNN and dlib.
+    """
+    detected_faces = model(_file, 2)
+    if detected_faces:
+        print("CNN: {0} faces".format(len(detected_faces)))
+    return len(detected_faces)
 
 
 if __name__ == "__main__":
