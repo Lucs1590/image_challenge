31e9860d1a5a2d39272b2fcbda83825201a5c988
Lucas Brito <lucasbsilva29@gmail.com>
2021-03-04 20:58:20 -0300

    test(test_aux.py): test to read and image size

--- tests/test_aux.py
+++ tests/test_aux.py
@@ -1,11 +1,24 @@
 import unittest
 import cv2
 
+import src.exercise1 as exercise
+
 
 class TestAuxiliaryUnit(unittest.TestCase):
     def setUp(self):
-        self.image_path = 'resources/images/image.jpg'
-        self.cv_image = cv2.imread(self.image_path)
+        self.image_path = 'tests/fixtures/test_img.jpg'
+        self.image = cv2.imread(self.image_path)
+
+    def test_img_read(self):
+        image = exercise.read_img(self.image_path)
+        self.assertIsNotNone(image)
+
+    def test_resize_image_width(self):
+        image = self.image
+        image_shape = image.shape
+        image_returned = exercise.change_size(image, 1200, 1200, 0.1)
+        image_returned_shape = image_returned.shape
+        self.assertNotEqual(image_shape, image_returned_shape)
 
 
 if __name__ == '__main__':
